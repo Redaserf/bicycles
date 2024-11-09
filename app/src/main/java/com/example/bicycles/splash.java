294fe5ddcc5300768e6fa214852ee66645ba65fb
Aaron Alejandro Martínez Solis <aaronjrale@gmail.com>
2024-11-08 20:27:25 -0600

    Diseño algo mejorado

---
+++ app/src/main/java/com/example/bicycles/splash.java
@@ -0,0 +1,32 @@
+package com.example.bicycles;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.os.CountDownTimer;
+
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new CountDownTimer(3000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+            }
+
+            @Override
+            public void onFinish() {
+                setContentView(R.layout.activity_login);
+            }
+        }.start();
+
+    }
+}
