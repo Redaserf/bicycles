efea87ddd2e025f7111e8778d6db1d54903db9d6
Aaron Alejandro Martínez Solis <aaronjrale@gmail.com>
2024-11-19 22:38:36 -0600

    [UPD]-Login y Registro

---
+++ app/src/androidTest/java/com/example/bicycles/Networks/ApiService.java
@@ -0,0 +1,20 @@
+package com.example.bicycles.Networks;
+
+
+
+import com.example.bicycles.Model.LoginRequest;
+import com.example.bicycles.Model.RegisterRequest;
+import com.example.bicycles.Responses.LoginResponse;
+import com.example.bicycles.Responses.RegisterResponse;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface ApiService {
+
+    @POST("api/v1/login")
+    Call<LoginResponse> login (@Body LoginRequest request);
+
+    @POST("api/v1/register")
+    Call<RegisterResponse> register (@Body RegisterRequest request);
+}
