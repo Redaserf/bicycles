12c94f11a7ee9d00205cf3a8814930050d3b1ff5
Anggel87 <angelheriberto87@gmail.com>
2024-11-26 14:32:38 -0600

    [UPD] - Recorridos
    Aún no funcional, creo

---
+++ app/src/main/java/com/example/bicycles/Repositories/RecorridoRepository.java
@@ -0,0 +1,4 @@
+package com.example.bicycles.Repositories;
+
+public class RecorridoRepository {
+}
--- app/src/main/java/com/example/bicycles/Singleton/RetrofitClient.java
+++ app/src/main/java/com/example/bicycles/Singleton/RetrofitClient.java
@@ -1,5 +1,8 @@
 package com.example.bicycles.Singleton;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 import com.example.bicycles.Networks.ApiService;
 
 import java.io.IOException;
@@ -12,23 +15,38 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static RetrofitClient instance = null ;
+    private static RetrofitClient instance = null;
     private ApiService apiService;
 
     private static final String BASE_URL = "http://192.168.1.10:8000/api/v1/";
 
+    private RetrofitClient(Context context) {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        SharedPreferences preferences = context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+                        String token = preferences.getString("auth_token", ""); // Obtén el token almacenado
+                        Request request = chain.request().newBuilder()
+                                .addHeader("Authorization", "Bearer " + token) // Agrega el token al header
+                                .build();
+                        return chain.proceed(request);
+                    }
+                })
+                .build();
 
-    private RetrofitClient(){
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build();
 
         apiService = retrofit.create(ApiService.class);
     }
-    public static synchronized RetrofitClient getInstance() {
+
+    public static synchronized RetrofitClient getInstance(Context context) {
         if (instance == null) {
-            instance = new RetrofitClient();
+            instance = new RetrofitClient(context);
         }
         return instance;
     }
