9be5a8fa26768e156a53e512a64b77f0193d0e02
Anggel87 <angelheriberto87@gmail.com>
2024-11-26 17:02:58 -0600

    [UPD] - ViewModel de bicicletas y recorridos
    Y arreglos menores

---
+++ app/src/main/java/com/example/bicycles/ViewModels/BicicletaViewModel.java
@@ -0,0 +1,27 @@
+package com.example.bicycles.ViewModels;
+
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.bicycles.Repositories.BicicletaRepository;
+
+public class BicicletaViewModel extends ViewModel {
+
+    private MutableLiveData<String> bicicletaMessage;
+    private BicicletaRepository bicicletaRepository;
+
+    public BicicletaViewModel() {
+        this.bicicletaRepository = new BicicletaRepository();
+    }
+
+    public void addBicicleta(String nombre, int usuario_id, Context context) {
+        bicicletaMessage = bicicletaRepository.addBicicleta(nombre, usuario_id, context);
+    }
+
+    public LiveData<String> getBicicletaMessage() {
+        return bicicletaMessage;
+    }
+}
---
+++ app/src/main/java/com/example/bicycles/ViewModels/RecorridoViewModel.java
@@ -0,0 +1,39 @@
+package com.example.bicycles.ViewModels;
+
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.bicycles.Models.Recorrido;
+import com.example.bicycles.Repositories.RecorridoRepository;
+
+public class RecorridoViewModel extends ViewModel {
+
+    private MutableLiveData<String> recorridoMessage;
+    private MutableLiveData<Recorrido> recorridoData;
+    private RecorridoRepository recorridoRepository;
+
+    public RecorridoViewModel() {
+        this.recorridoRepository = new RecorridoRepository();
+    }
+
+    public void addRecorrido(int usuario_id, int bicicleta_id, String tiempo, double calorias,
+                             double velocidadPromedio, double velocidadMaxima, double distanciaRecorrida, Context context) {
+        recorridoMessage = recorridoRepository.addRecorrido(usuario_id, bicicleta_id, tiempo, calorias,
+                velocidadPromedio, velocidadMaxima, distanciaRecorrida, context);
+    }
+
+    public LiveData<String> getRecorridoMessage() {
+        return recorridoMessage;
+    }
+
+    public void fetchRecorridoById(int recorridoId, Context context) {
+        recorridoData = recorridoRepository.getRecorridoById(recorridoId, context);
+    }
+
+    public LiveData<Recorrido> getRecorridoData() {
+        return recorridoData;
+    }
+}
