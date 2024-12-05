c4960a91839328bc6da815b7eda64746985b0a5a
Redaserf <113316682+Redaserf@users.noreply.github.com>
2024-12-05 13:15:47 -0600

    recorridos

--- app/src/main/java/com/example/bicycles/Responses/BicicletaResponse.java
+++ app/src/main/java/com/example/bicycles/Responses/BicicletaResponse.java
@@ -1,11 +1,22 @@
 package com.example.bicycles.Responses;
 
-import com.example.bicycles.Models.Bicicleta;;
+import com.example.bicycles.Models.Bicicleta;
+import com.example.bicycles.Models.Recorrido;;import java.util.List;
 
 public class BicicletaResponse {
 
     private String mensaje;
     private Bicicleta bicicleta;
+
+    private List<Recorrido> recorridos;
+
+    public List<Recorrido> getRecorridos() {
+        return recorridos;
+    }
+
+    public void setRecorridos(List<Recorrido> recorridos) {
+        this.recorridos = recorridos;
+    }
 
     public String getMensaje() {
         return mensaje;
