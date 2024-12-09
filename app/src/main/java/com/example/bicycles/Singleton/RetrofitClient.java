46f9a5152e330583beebc399d34a4abf74a99358
Redaserf <113316682+Redaserf@users.noreply.github.com>
2024-12-09 11:55:13 -0600

    [ADD]- filtro en iniciar recorrido de bicis

--- app/src/main/java/com/example/bicycles/Singleton/RetrofitClient.java
+++ app/src/main/java/com/example/bicycles/Singleton/RetrofitClient.java
@@ -18,7 +18,7 @@ public class RetrofitClient {
     private static RetrofitClient instance = null;
     private ApiService apiService;
 
-    public static final String BASE_URL = "http://192.168.252.161:8000/api/v1/";
+    public static final String BASE_URL = "http://3.135.19.191/api/v1/";
 
     private RetrofitClient(Context context) {
         // Crear OkHttpClient con un interceptor que lea el token dinámicamente
